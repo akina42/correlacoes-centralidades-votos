09cb46e7321ba48dde59186608f33475d4fc5edf
akina42 <akina_ak@hotmail.com>
2018-09-11 18:43:03 -0300

    Add - Exportação dos resultados

--- Scripts_R/Script_Centralidade_Grau.R
+++ Scripts_R/Script_Centralidade_Grau.R
@@ -4,14 +4,16 @@ dados.grau <- read.csv("Dados/votos_governador_degree_centrality.csv", header=F,
 attach(dados.grau)
 
 cor(V2, V3, method="spearman")
-
 cor.s = cor.test(V2, V3, method="spearman", exact=F)
-
 cor.s
 
-plot(V2, V3)
+saveRDS(cor.s, "Resultados/votos_governador_degree_centrality.rds")
 
+plot(V2, V3)
 abline(lm(V3 ~ V2))
+
+correlacao <- readRDS("Resultados/votos_governador_degree_centrality.rds")
+correlacao
 
        
        
